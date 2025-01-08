218e34d1e283080fb6b15cae85bce2697ab795e3
Joseph <JosephOgbo18@outlook.com>
2025-01-08 17:15:08 +0000

    tested format currency function

---
+++ 13-javascript-amazon-project copy - Copy/tests-jasmine/moneyTest.js
@@ -0,0 +1,15 @@
+import {formatCurrency} from '../scripts/utils/money.js'
+
+describe('Test Suite: formatCurrecny', () => {
+  it('coverts cents into dollars', () => {
+    expect(formatCurrency(2095)).toEqual('20.95');
+  });
+
+  it('works with 0', () => {
+    expect(formatCurrency(0)).toEqual('0.00')
+  });
+
+  it('works with rounding up', () => {
+    expect(formatCurrency(2095.5)).toEqual('20.96')
+  });
+});
